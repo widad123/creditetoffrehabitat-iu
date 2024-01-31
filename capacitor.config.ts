27aca4a0da8322232e778da08872e4cd0904f7f1
widad123 <khatiri.widad@gmail.com>
2024-01-31 11:40:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'creditetoffrehabitat-iu',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
